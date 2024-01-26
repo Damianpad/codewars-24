c8cd4579a03605018ce9189961de1dafc62160da
Damian Padilla <damipad4@gmail.com>
2024-01-25 19:30:45 -0600

    completed splice problem

--- Sandbox/splice.js
+++ Sandbox/splice.js
@@ -6,3 +6,26 @@ function removeLast2(arr){
 }
 
 console.log(removeLast2(myArr));
+
+// Remove all element swith values greater than 10
+let myGreatArr = [122,20,3,34,5,6,7,88,9,10];
+
+function removeMore10(arr){
+    for(let i = arr.length - 1; i >= 0; i--){
+        if(arr[i] > 10){
+            arr.splice(i, 1)
+        }
+    }
+
+    return arr;
+}
+
+console.log(removeMore10(myGreatArr));
+
+
+// splice MODIFIES arrays. Here is an example
+let mySimpleArr = [1,2,3,4,5]
+
+let newArr = mySimpleArr.splice(1,1)
+console.log(newArr); // Returns [2]
+console.log(mySimpleArr); // Returns [1,3,4,5]
