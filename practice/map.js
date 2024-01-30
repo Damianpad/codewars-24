3b38fee7b9b806df89397562c010d991931938e7
Damian Padilla <damipad4@gmail.com>
2024-01-30 12:51:53 -0600

    completed problem 1 for map

---
+++ practice/map.js
@@ -0,0 +1,7 @@
+// PROBLEM 1: Given an array of numbers, use .map() to create a new array where each number is doubled.
+
+const nums = [1,2,3,4,5];
+
+let doubleNums = nums.map(number => number * 2);
+
+console.log(doubleNums);
