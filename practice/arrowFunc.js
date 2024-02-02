bb34008a4ee7234bf742344400b5f1462685e0f6
Damian Padilla <damipad4@gmail.com>
2024-02-01 23:49:45 -0600

    completed arrow function conversion problems

--- practice/arrowFunc.js
+++ practice/arrowFunc.js
@@ -1,3 +1,82 @@
+// Arrow functions the basics
+
+//let func = (args1, args2 ) => expression
+
+/*
+Convert these basic functions into an arrow function
+*/
+
+/*
+Convert 1
+
+function add( a, b ){
+    return a + b;
+}
+*/
+
+const add = (a , b) => a + b;
+
+/*
+Convert 2: Subtraction
+
+function subtract(a, b){
+    return a - b;
+}
+*/
+
+const subtract = (a,b) => a - b;
+
+/*
+Convert 3: Multiplication
+
+function multiply(a, b){
+    return a * b;
+}
+*/
+
+const multiply = (a, b) => a * b;
+
+/*
+Convert 4: Division
+function divide(a,b){
+    return a / b;
+}
+*/
+
+const divide = (a,b) => a / b;
+
+/*
+Convert 5: Square
+
+function square(x) {
+    return x * x;
+}
+*/
+
+const square = x => x * x;
+
+/*
+Convert 6: Cubed
+
+function cube(x){
+    return x * x * x;
+}
+*/
+
+const cubed = x => x * x * x;
+
+/*
+Convert 7: Check if even functions
+
+function isEven(num){
+    return num % 2 === 0;
+}
+*/
+
+let isEven = num => num % 2 === 0;
+
+
+
 // PROBLEM 1: Given an array of numbers, use an arrow function to create a new array where each number is doubled.
 
 let nums = [1,2,3,4,5];
