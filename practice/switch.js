5bbaf4b6158af4334a5cf9fc7f782ce16e126f62
Damian Padilla <damipad4@gmail.com>
2024-02-05 17:21:48 -0600

    completed firstLast2

--- practice/switch.js
+++ practice/switch.js
@@ -1,37 +1,37 @@
 // Use a switch statement when you want to compare a variable to a bunch of different values
 
-let favoriteColor = 'Navy';
+let favoriteColor = "Navy";
 
-switch(favoriteColor){
-    case 'Red':
-        console.log("Nope way too Red");
-        break;
-    case 'Navy':
-        console.log("PERFECT");
-        break;
-    case 'Green':
-        console.log("Nope way too Green");
-        break;
-    default:
-        console.log("Sorry I don't know that color");
+switch (favoriteColor) {
+  case "Red":
+    console.log("Nope way too Red");
+    break;
+  case "Navy":
+    console.log("PERFECT");
+    break;
+  case "Green":
+    console.log("Nope way too Green");
+    break;
+  default:
+    console.log("Sorry I don't know that color");
 }
 
 // PROBLEM 1: Create a function that takes a color 'R' 'G' 'B' and returns the primary color.
 
-const color = letter => {
-    switch(letter){
-        case 'R':
-            console.log('Red');
-            break;
-        case 'G':
-            console.log('Green');
-            break;
-        case 'B':
-            console.log('Blue');
-            break;
-    }
-}
+const color = (letter) => {
+  switch (letter) {
+    case "R":
+      console.log("Red");
+      break;
+    case "G":
+      console.log("Green");
+      break;
+    case "B":
+      console.log("Blue");
+      break;
+  }
+};
 
-console.log(color('R'));
-console.log(color('G'));
-console.log(color('B'));
+console.log(color("R"));
+console.log(color("G"));
+console.log(color("B"));
