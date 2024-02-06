9dba2061819b0fb58398d7c6de0e631c52734820
Damian Padilla <damipad4@gmail.com>
2024-02-05 23:53:24 -0600

    completed fundReturn

---
+++ daily/daily.js
@@ -0,0 +1,25 @@
+// Arrays
+
+/*
+Splice:
+
+Problem 1: remove the last two elements from an array
+*/
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+/*
+Splice:
+
+Problem 2: Given an array of numbers, remove all elements with values greater than 10
+*/
+let largeArr = [3, 50, 4, 55, 77, 88, 2, 4, 1, 100];
+
+/*
+Splice:
+
+Problem 3: Given an array of strings, remove the first three elements.
+*/
+
+let strArr = ["Cat", "Dog", "Bird", "Monkey", "Horse"];
+
+
