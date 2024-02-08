72f9f401635b6f072a27db0ab75510bf80827d0b
Damian Padilla <damipad4@gmail.com>
2024-02-08 16:34:52 -0600

    completed stringRepeat

---
+++ stringRepeat.js
@@ -0,0 +1,20 @@
+/*
+Write a function that accepts an integer n and a string s as parameters, and returns a string of s repeated exactly n times.
+
+Examples (input -> output)
+6, "I"     -> "IIIIII"
+5, "Hello" -> "HelloHelloHelloHelloHello"
+
+Integer n
+String s
+
+A string of s repeated n times
+
+6, "I" => "IIIIII"
+
+s.repeat(n);
+*/
+
+function repeatStr (n, s) {
+  return s.repeat(n);
+}
