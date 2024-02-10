eb95fb4d1a0d96144401ea69394a3b10895df581
Damian Padilla <damipad4@gmail.com>
2024-02-10 09:39:48 -0600

    completed map problem and daily warmup

--- daily/daily.js
+++ daily/daily.js
@@ -7,6 +7,9 @@ Problem 1: remove the last two elements from an array
 */
 let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+arr.splice(-2);
+console.log(arr);
+
 /*
 Splice:
 
@@ -22,7 +25,8 @@ Problem 3: Given an array of strings, remove the first three elements.
 
 let strArr = ["Cat", "Dog", "Bird", "Monkey", "Horse"];
 
-
+strArr.splice(0, 3);
+console.log(strArr);
 
 /*
 Map:
@@ -31,8 +35,22 @@ Problem 1: Given an array of integers, return a new array with each value double
 
 */
 
-let arrInts = [1,2,3];
+let arrInts = [1, 2, 3];
 
-const maps= (x) => {
-    return x.map(element => element * 2);
-}
+const doubled = (x) => {
+  return x.map((element) => element * 2);
+};
+
+console.log(doubled(arrInts));
+
+/*
+Map:
+
+Problem 2: Given an array of numbers, create a new array where each element is the square of the corresponding element in the original array
+*/
+
+const squared = (x) => {
+  return x.map((element) => element ** 2);
+};
+
+console.log(squared(arrInts));
