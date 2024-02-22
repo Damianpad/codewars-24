cd5d75b5f728fd4424ef3c501f7503a6f56a16bc
Damian Padilla <damipad4@gmail.com>
2024-02-22 16:41:33 -0600

    completed array destructuring example

---
+++ practice/destructuring.js
@@ -0,0 +1,9 @@
+// Example of Array destructuring
+
+const favFoods = ["bacon", "pizza", "fries"];
+
+const [ana, , damian] = favFoods;
+
+console.log(ana);
+console.log(damian);
+// console.log(lizzy);
