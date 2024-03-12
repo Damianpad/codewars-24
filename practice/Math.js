263f13cd3bd5aa55a44ae80c3f6f3b5bc8ccf0ee
Damian Padilla <damipad4@gmail.com>
2024-03-12 13:03:57 -0500

    added Math practice

---
+++ practice/Math.js
@@ -0,0 +1,28 @@
+// Math is a built in object that provides a collection of properties and methods
+
+// Here are some basic arithmetic methods
+
+// Math.abs() returns the absolute value of a number 'x'
+console.log(Math.abs(2)); // returns 2
+console.log(Math.abs(-2)); // returns 2
+
+console.log(Math.abs(5)); // returns 5
+console.log(Math.abs(-5)); // returns 5
+
+// Math.ceil() rounds a number up to nearest integer
+console.log(Math.ceil(1.2)); // returns 2
+console.log(Math.ceil(5.7)); // returns 6
+
+// Math.floor() rounds a number down to nearest integer
+console.log(Math.floor(4.5)); // returns 4
+console.log(Math.floor(1.1)); // returns 1
+
+// Math.max() returns the largest of zero or more numbers
+console.log(Math.max()); // returns -Infinity
+console.log(Math.max(2, 44, 0, 100, 23)); // returns 100
+console.log(Math.max(-1, -33, 0)); // returns 0
+
+// Math.min() returns the smallest of zero or more numbers
+console.log(Math.min()); // returns Infinity
+console.log(Math.min(2, 44, 0, 100, 23)); // returns 0
+console.log(Math.min(-1, -33, 0)); // returns 33
